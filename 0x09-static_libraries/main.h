dc37a4830606510962d19927dcb169e749ff0015
leworkalush <lework03@gmail.com>
2023-01-06 21:22:47 +0300

    done

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,4 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+#endif
